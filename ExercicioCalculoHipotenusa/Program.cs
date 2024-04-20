20ccee762beee28bad550f79e9c9d10025f9b05a
gabsoares <soaresgabriel68@gmail.com>
2024-04-20 00:23:24 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioCalculoHipotenusa/Program.cs
@@ -0,0 +1,10 @@
+﻿double cateto1, cateto2, hipotenusa;
+
+Console.Write("Digite o valor do primeiro cateto: ");
+cateto1 = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor do segundo cateto: ");
+cateto2 = double.Parse(Console.ReadLine());
+hipotenusa = Math.Sqrt((cateto1 * cateto1) + (cateto2 * cateto2));
+
+Console.WriteLine($"O valor da hipotenusa é: {hipotenusa}");
